6205c42a001827154c438c97c81b2be8e02b1eee
Mantvydas Šemeta <mantvydassemeta@gmail.com>
2024-09-10 16:27:33 +0300

    Starting Service

---
+++ NariuDuomenuBaze.Core/Contracts/IMemberService.cs
@@ -0,0 +1,32 @@
+﻿using NariuDuomenuBaze.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NariuDuomenuBaze.Core.Contracts
+{
+    public interface IMemberService
+    {
+        Task<List<Member>> GetAllMembers();
+
+        Task AddMember(Member member);
+
+        Task UpdateMember(Member member);
+
+
+        //Find
+        Task<Member> GetMemberBySurname(string surname);
+
+
+        //Delete
+        Task DeleteMemberById(int id);
+        Task DeleteMemberBySurname(string surname);
+
+
+
+
+
+    }
+}
--- NariuDuomenuBaze.Core/Services/MemberService.cs
+++ NariuDuomenuBaze.Core/Services/MemberService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NariuDuomenuBaze.Core.Contracts;
+using NariuDuomenuBaze.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,53 @@ using System.Threading.Tasks;
 
 namespace NariuDuomenuBaze.Core.Services
 {
-    public class MemberService
+    public class MemberService : IMemberService
     {
+        private readonly IMemberEfDbRepository _memberEfDbRepository;
+        private readonly IMemberFileRepository _memberFileRepository;
+
+        public MemberService(IMemberEfDbRepository efDbRepository, IMemberFileRepository memberFileRepository)
+        {
+            _memberEfDbRepository = efDbRepository;
+            _memberFileRepository = memberFileRepository;
+        }
 
 
 
+        public async Task AddMember(Member member)
+        {
+            await _memberEfDbRepository.AddMember(member);
+            await _memberFileRepository.AddMember(member);
+        }
+
+        public async Task DeleteMemberById(int id)
+        {
+            await _memberEfDbRepository.DeleteMemberById(id);
+            await _memberFileRepository.DeleteMemberById(id);
+
+        }
+
+        public async Task DeleteMemberBySurname(string surname)
+        {
+            await _memberEfDbRepository.DeleteMemberBySurname(surname);
+            await _memberFileRepository.DeleteMemberBySurname(surname);
+        }
+
+        public async Task<List<Member>> GetAllMembers()
+        {
+            return await _memberEfDbRepository.GetAllMembers();
+        }
+
+        public async Task<Member> GetMemberBySurname(string surname)
+        {
+            return await _memberEfDbRepository.GetMemberBySurname(surname);
+        }
+
+        public async Task UpdateMember(Member member)
+        {
+            await _memberEfDbRepository.UpdateMember(member);
+            await _memberFileRepository.UpdateMember(member);
+
+        }
     }
 }
